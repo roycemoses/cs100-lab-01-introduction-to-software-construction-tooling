c93c78922bbcb315db49dc5ec2386bd9b9f3d5c4
roycemoses <royce.kmoses@gmail.com>
2020-10-04 19:38:31 -0700

    Add rectangle area program
    * Added Rectangle class files and mainf ile that calculates the rectangle's area

---
+++ src/rectangle.cpp
@@ -0,0 +1,16 @@
+#include "../header/rectangle.hpp"
+
+void Rectangle::set_width(int w)
+{
+	this->width = w;
+}
+
+void Rectangle::set_height(int h)
+{
+	this->height = h;
+}
+
+int Rectangle::area()
+{
+	return this->width * this->height;
+}
