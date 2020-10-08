5ecc57639e01d8beae0fb97a418d972e1f40829e
roycemoses <royce.kmoses@gmail.com>
2020-10-07 23:49:37 -0700

    add three new edge cases

--- src/test.cpp
+++ src/test.cpp
@@ -8,6 +8,32 @@ TEST(EchoTest, HelloWorld)
 	EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString)
+{
+	char* test_val[1]; test_val[0] = "./c-echo";
+	EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, SpecialChars)
+{
+	char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "...hello!"; test_val[2] = "*world*???";
+	EXPECT_EQ("...hello! *world*???", echo(3,test_val));
+}
+
+TEST(EchoTest, ThreeSpaces)
+{
+	char* test_val[4]; test_val[0] = "./c-echo"; test_val[1] = " "; test_val[2] = " "; test_val[3] = " ";
+	EXPECT_EQ("     ", echo(4,test_val));
+}
+
+TEST(EchoTest, SpecialCharsAndTabs)
+{
+	char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "	%^&)(>><<%%@@?&"; test_val[2] = ":)||	";
+	EXPECT_EQ("	%^&)(>><<%%@@?& :)||	", echo(3,test_val));
+}
+
+
+
 int main(int argc, char** argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
